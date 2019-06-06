80c20c0803acd5585765771248ab7a9c9312100e
Wei Du <wei.du.2015@marshall.usc.edu>
2019-06-06 10:58:52 -0400

    testing nested statements

--- server.js
+++ server.js
@@ -14,37 +14,41 @@ app.get('*', (req, res) => {
 
 app.post('*', (req, res) => {
   let {sessionId, serviceCode, phoneNumber, text} = req.body
-  if (text == '') {
-    // This is the first request. Note how we start the response with CON
-    let response = `CON What would you want to check
-    1. My Account
-    2. My phone number`
-    res.send(response)
-  } else if (text == '1') {
-    // Business logic for first level response
-    let response = `CON Choose account information you want to view
-    1. Account number
-    2. Account balance`
-    res.send(response)
-  } else if (text == '2') {
-    // Business logic for first level response
-    let response = `END Your phone number is ${phoneNumber}`
-    res.send(response)
-  } else if (text == '1*1') {
-    // Business logic for first level response
-    let accountNumber = 'ACC1001'
-    // This is a terminal request. Note how we start the response with END
-    let response = `END Your account number is ${accountNumber}`
-    res.send(response)
-  } else if (text == '1*2') {
-    // This is a second level response where the user selected 1 in the first instance
-    let balance = 'NGN 10,000'
-    // This is a terminal request. Note how we start the response with END
-    let response = `END Your balance is ${balance}`
-    res.send(response)
-  } else {
-    res.status(400).send('Bad request!')
+
+  switch(text) {
+    case '':
+      response= `CON Welcome to Sauti Marketplace, country selection 
+      1. Keyna
+      2. take me home
+      `;
+      break;
+    case '1':
+      response=`CON choose your market
+      1. 1st
+      2. 2nd
+      3. 3rd
+      `;
+      break;
+    case '2':
+      response=`END goodbye`;
+      break;
+    case '1*1':
+      response=`CON what do you want to purchase?
+      1. potato
+      2. unicorn
+      `;
+    break;
+    case '1*1*1':
+      response=`END potato is $200/ea today, pretty expensive`;
+      break;
+    case '1*1*2':
+      response=`END unicorn is out of stock today, please try tomorrow`;
+      break;  
+    
+    default:
+      response = 'Please try again'  
   }
+  res.send(response)
 })
 
 app.listen(port, () => {
