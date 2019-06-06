810c66ccc73d9bf27b046a6d07827bcd43f84a75
Wei Du <wei.du.2015@marshall.usc.edu>
2019-06-06 16:31:28 -0400

    adj var countries line 15

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(logger('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 
-const countries = app.get('/', async (req, res) => {
+const countries = app.get('*', async (req, res) => {
    try{
      const countries = await Countries.get();
      res.status(200).json(countries)
