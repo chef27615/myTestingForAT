b65b03a92e76829cfdc15d7d7d6577aa5d9c2fde
Wei Du <wei.du.2015@marshall.usc.edu>
2019-06-07 11:55:03 -0400

    remove req.body name var

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.get('*', (req, res) => {
 
 app.post('*', (req, res) => {
   let name = 'kenya'
-  let {sessionId, serviceCode, phoneNumber, name, text} = req.body
+  let {sessionId, serviceCode, phoneNumber, text} = req.body
 
   switch(text) {
     case '':
